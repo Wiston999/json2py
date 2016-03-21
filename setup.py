80fe14287e996a32696c474fc2b9d1b5d4335970
Victor Cabezas <wiston666@gmail.com>
2016-03-21 23:15:45 +0100

    Added setup.py to repository

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name = 'json2py',
+    version = '0.1',
+    packages = ['json2py'],
+    url = 'https://github.com/Wiston999/json2py',
+    license = '',
+    author = 'Victor Cabezas',
+    author_email = 'wiston666@gmail.com',
+    description = 'Convert JSON/dict to python object and viceversa'
+)
